c03bded0010a72df3b681dee06f5bb332ca4538d
Viktor <viktorpar@gmail.com>
2018-08-02 21:44:31 +0300

    Blog functionality and polish

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,17 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    def summary(self):
+        if len(self.body) > 100:
+            return (self.body[:100]+'...')
+        self.body
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
+
 # Add Blog app to the settings - ok
 
 # Create a migration - python manage.py makemigrations - ok
--- jobs/views.py
+++ jobs/views.py
@@ -6,5 +6,4 @@ from .models import Job
 
 def home(request):
     jobs = Job.objects
-    print(jobs)
     return render(request, 'jobs/home.html', {'jobs': jobs})
